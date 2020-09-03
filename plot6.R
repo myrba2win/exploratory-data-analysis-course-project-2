783337eedff2b45b6265bbb01f3dd1f025e4f6d2
myrba2win <myrba2win@yahoo.com>
2020-09-03 01:36:15 -0400

    Uploading Files.

---
+++ plot6.R
@@ -0,0 +1,55 @@
+# Read the base zip file "exdata_data_NEI_data.zip" that contains the data
+## This first line will likely take a few seconds. Be patient!
+
+dataInput_file <- "exdata_data_NEI_data.zip"
+if (!file.exists(dataInput_file)) {
+  data_URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+  download.file(data_URL, destfile = dataInput_file)
+  unzip (zipfile = dataInput_file)
+}
+
+
+
+# Verify if "summarySCC_PM25.rds" file was downloaded before. If not, just do it.
+if (!exists("NEI")) {
+  NEI <- readRDS("summarySCC_PM25.rds")
+}
+dim(NEI)
+
+# Verify if "Source_Classification_Code.rds" file was downloaded before. If not, just do it.
+if (!exists("SCC")) {
+  SCC <- readRDS("Source_Classification_Code.rds")
+}
+dim(SCC)
+
+
+
+
+library(ggplot2)
+
+## Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
+
+## Subset coal combustion related NEI data
+
+
+png("plot6.png", width=640, height=480)
+require(dplyr)
+baltcitymary.emissions<-summarise(group_by(filter(NEI, fips == "24510"& type == 'ON-ROAD'), year), Emissions=sum(Emissions))
+losangelscal.emissions<-summarise(group_by(filter(NEI, fips == "06037"& type == 'ON-ROAD'), year), Emissions=sum(Emissions))
+
+baltcitymary.emissions$County <- "Baltimore City, MD"
+losangelscal.emissions$County <- "Los Angeles County, CA"
+both.emissions <- rbind(baltcitymary.emissions, losangelscal.emissions)
+
+require(ggplot2)
+c_plot <- ggplot(both.emissions, aes(x=factor(year), y=Emissions, fill=County,label = round(Emissions,2))) +
+  geom_bar(stat="identity") + 
+  facet_grid(County~., scales="free") +
+  ylab(expression("total PM"[2.5]*" emissions in tons")) + 
+  xlab("year") +
+  ggtitle(expression("Motor vehicle emission variation in Baltimore and Los Angeles in tons"))+
+  geom_label(aes(fill = County),colour = "white", fontface = "bold")
+
+print(c_plot)
+
+dev.off()
