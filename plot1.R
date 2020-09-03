29326336f0aab9188ce7ee1ae8c40f9c5d6b2f2b
myrba2win <myrba2win@yahoo.com>
2020-09-03 00:02:05 -0400

    Updating files.

--- plot1.R
+++ plot1.R
@@ -15,9 +15,28 @@ if (!exists("NEI")) {
   NEI <- readRDS("summarySCC_PM25.rds")
 }
 dim(NEI)
+head(NEI)
 
 # Verify if "Source_Classification_Code.rds" file was downloaded before. If not, just do it.
 if (!exists("SCC")) {
   SCC <- readRDS("Source_Classification_Code.rds")
 }
 dim(SCC)
+head(SCC)
+
+##  Have total emissions from PM2.5 decreased in the United States from 
+##  1999 to 2008? Using the base plotting system, make a plot showing the 
+##  total PM2.5 emission from all sources for each of the years 1999, 2002, 
+##  2005, and 2008.
+
+total_emissions_PM25 <- aggregate(Emissions ~ year, NEI, sum)
+total_emissions_PM25
+
+png('plot1.png')
+barplot(height=total_emissions_PM25$Emissions/1000, 
+        names.arg=total_emissions_PM25$year, 
+        xlab="years", 
+        ylab=expression('total PM'[2.5]*' emission in KiloTons'),
+        main=expression('Total PM'[2.5]*' emissions at various years'))
+dev.off()
+
